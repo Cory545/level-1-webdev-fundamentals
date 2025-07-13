a2f5c654b3868c14076042b1f185bcdda889777d
Cory545 <Coryburton545@gmail.com>
2025-07-13 13:36:42 +0100

    Create simple arithmetics. Addition, divide, multiply, minus. Needs cleaning and formatting better.

--- simple_arithmetic/script.js
+++ simple_arithmetic/script.js
@@ -1,4 +1,4 @@
-//This was my first attempt
+//This was my first play around
 let x = 1;
 let y = 2;
 let z = 0;
@@ -7,8 +7,29 @@ function addition(){
 };
 addition();
 console.log(z);
-//My Second attempt
+//My Second play around
 function addition2(a, b){
     return a + b
 };
 console.log(addition2(1, 2));
+//Third play around
+
+function addition3(h, f){
+    return h + f;
+};
+console.log(addition3(10, 20));
+
+function divide(h, f){
+    return h / f;
+};
+console.log(divide(20, 10))
+
+function multiply(h, f){
+    return h * f;
+};
+console.log(multiply(10, 20));
+
+function minus(h, f){
+    return h - f;
+};
+console.log(minus(10, 20));
