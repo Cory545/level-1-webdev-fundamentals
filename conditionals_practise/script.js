273926d00072a007c27441491dc57cd11b1fc2a9
Cory545 <Coryburton545@gmail.com>
2025-07-13 14:04:32 +0100

    First play with conditionals

---
+++ conditionals_practise/script.js
@@ -0,0 +1,36 @@
+//First trial.
+//Trying to do a simple if and else statement
+let number = 50;
+let secondNumber = 93;
+if (number == 50){
+    console.log("The number is 50")
+};
+if (secondNumber == 50){
+    console.log("The number is 50")
+} else{
+    console.log("The number must be 93")
+};
+//Now trying to do a if, else if and else
+let thirdNumber = 60;
+if (thirdNumber == 50){
+    console.log("The number is 50")
+} else if(thirdNumber == 60){
+    console.log("The number is 60")
+} else{
+    console.log("Idk man")
+};
+//Now doing if statements, but not just ==
+let fourthNumber = 18;
+if(fourthNumber >= 18){
+    console.log("Congratulations, you can vote.")
+} else{
+    console.log("Sorry. You can't vote.")
+};
+let fifthNumber = 73;
+if (fifthNumber >= 90 && fifthNumber <= 100){
+    console.log("Your grade was A")
+} else if(fifthNumber >= 80 && fifthNumber <= 89){
+    console.log("Your grade is B")
+} else{
+    console.log("Failed basically.")
+};
