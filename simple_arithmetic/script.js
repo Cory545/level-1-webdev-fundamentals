c4ce0855ed989f5ca680089996c67ea356bb25fa
Cory545 <Coryburton545@gmail.com>
2025-07-13 13:25:39 +0100

    Complete second trial of simple addition. Got rid of global variables, used argument instead.

--- simple_arithmetic/script.js
+++ simple_arithmetic/script.js
@@ -2,10 +2,13 @@
 let x = 1;
 let y = 2;
 let z = 0;
-
 function addition(){
     return z = x + y
 };
-
 addition();
 console.log(z);
+//My Second attempt
+function addition2(a, b){
+    return a + b
+};
+console.log(addition2(1, 2));
