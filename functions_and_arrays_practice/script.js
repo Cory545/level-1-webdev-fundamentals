6658a4365a0037e5617f8915388cf2576983042b
Cory545 <Coryburton545@gmail.com>
2025-07-15 20:51:32 +0100

    Complete first challenge

--- functions_and_arrays_practice/script.js
+++ functions_and_arrays_practice/script.js
@@ -1,1 +1,12 @@
 //First Session
+
+//Challenge. Function that accepts an array as a parameter. printGroceries(["Eggs", "Milk"]) for example
+
+
+function printGroceries(items){
+    for (let i = 0; i < items.length; i++){
+        console.log((i+1) + ". " + items[i])
+    }
+};
+
+printGroceries(["Eggs", "Milk", "Cheese"]);
