c4d85e509422f7baa4dd82015a58379297ff4fec
Cory545 <Coryburton545@gmail.com>
2025-07-17 21:47:46 +0100

    Complete Challenge 3

--- functions_and_arrays_practice/script.js
+++ functions_and_arrays_practice/script.js
@@ -12,9 +12,9 @@ function printGroceries(items){
 printGroceries(["Eggs", "Milk", "Cheese"]);
 
 //Challenge 2. Two arrays, one for items and one for prices. Loop through both, print out each item with its corresponding price. At the end, print out the total cost.
-let groceries = ["Eggs", "Milk", "Cheese"];
-let groceryOrder = [1, 2, 3];
-let groceryCost = [1.50, 1.00, 2.00];
+let groceries = ["Eggs", "Milk", "Cheese", "Bread"];
+let groceryOrder = [1, 2, 3, 4];
+let groceryCost = [1.50, 1.00, 2.00, 0.50];
 let sum = 0;
 
 function bothGroceries(order, items, prices) {
@@ -27,5 +27,20 @@ function bothGroceries(order, items, prices) {
 
 bothGroceries(groceryOrder, groceries, groceryCost);
 
+
 //Challenge 3. If the total exceeds £5, apply a discount.
+function bothGroceries(order, items, prices) {
+    for (let i = 0; i < order.length; i++) {
+        console.log(order[i] + ". " + items[i] + " - £" + prices[i]);
+        sum += prices[i];
+    }
+    if(sum >= 5){
+        let discountedSum = sum - (sum * 0.10)
+        console.log("Your total is = £" + discountedSum.toFixed(2));
+    } else {
+        console.log("Your total is = £" + sum.toFixed(2));
+
+}};
+
+bothGroceries(groceryOrder, groceries, groceryCost);
 //Challenge 4. Quantity logic. Each item now needs a quantity, and the price needs to reflect this.
