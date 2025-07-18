265f34eee0dc79e15bce81df9022191374efe6e9
Cory545 <Coryburton545@gmail.com>
2025-07-18 22:34:00 +0100

    Set up challenges for Objects

---
+++ objects_practise/script.js
@@ -0,0 +1,10 @@
+//Challenge 1. Create basic objects
+//Challenge 2. Access Object properties. such as object.key
+//Challenge 3. Update, delete and add keys to exisitng objects.
+//Challenge 4. Loop through an object.
+//Challenge 5. Create an array of grocery item objects, loop through name, price and quantities
+//Challenge 6. Calculate object total costs. Quantities * price
+//Challenge 7. Create an object with nested objects inside. Grocery store with sections like dairy, bakery etc.
+//Challenge 8. Create a method inside an object (a function as a property)
+//Challenege 9. Rebuild my grocery list from functions_and_arrays_practise, using array of objects instead of parralel arrays. Logic for discount if total > 5, in stock or out of stock.
+//Challenge 10. Understand what this refers to inside and object method.
