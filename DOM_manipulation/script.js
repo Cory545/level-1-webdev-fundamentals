dc25c60e2f844826de21f74f2b06708f2981d2eb
Cory545 <Coryburton545@gmail.com>
2025-07-20 21:55:07 +0100

    Complete selecting elements

--- DOM_manipulation/script.js
+++ DOM_manipulation/script.js
@@ -1,5 +1,8 @@
 // Selecting Elements
-
+    console.log(document.querySelector("#title"));
+    console.log(document.querySelectorAll("h2"))
+    console.log(document.querySelectorAll("h2")[0])
+    console.log(document.getElementById("title"));
 // Changing Content
 
 // Changing Style
