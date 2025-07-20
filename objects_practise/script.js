256b29116cbbde7c79193b154083a5d698f475bd
Cory545 <Coryburton545@gmail.com>
2025-07-20 10:55:57 +0100

    Challenge 7, cleared

--- objects_practise/script.js
+++ objects_practise/script.js
@@ -61,6 +61,8 @@ groceryList.forEach((item, index) =>{
     console.log(`${index + 1}. ${item.item} - £${item.price * item.quantity}`)
 })
 //Challenge 7. Create an object with nested objects inside. Grocery store with sections like dairy, bakery etc.
+//Already did this challenge
 //Challenge 8. Create a method inside an object (a function as a property)
+
 //Challenege 9. Rebuild my grocery list from functions_and_arrays_practise, using array of objects instead of parralel arrays. Logic for discount if total > 5, in stock or out of stock.
 //Challenge 10. Understand what this refers to inside and object method.
