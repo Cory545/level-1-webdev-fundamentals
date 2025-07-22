65b8ae1b592c84155955de72725ed9f0ed15e8af
Cory545 <Coryburton545@gmail.com>
2025-07-22 19:35:27 +0100

    Add CSS file, begin Changing Style section

--- DOM_manipulation/script.js
+++ DOM_manipulation/script.js
@@ -4,8 +4,13 @@
     console.log(document.querySelectorAll("h2")[0])
     console.log(document.getElementById("title"));
 // Changing Content
-
+document.querySelector("#title").innerHTML = "<h2>Checking if I did innerHTML well</h2>"
+document.querySelector("p").innerText = "This is a paragraph"
+document.getElementById("messageBox").innerHTML = "<h3><strong>Success!</strong></h3>"
 // Changing Style
+document.querySelector("#title").style.color = "red";
+document.querySelector("#title").style.backgroundColor = "gray";
+
 
 // Handling Events
 
